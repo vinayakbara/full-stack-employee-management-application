7e40c9ad4b6fa7051bd5d5376b80848e5d68d53a
vinayakbara <bara.vinayak09@gmail.com>
2025-01-30 10:31:23 +0530

    Commit

--- ems-backend/springboot-backend/src/main/java/springboot/controller/EmployeeController.java
+++ ems-backend/springboot-backend/src/main/java/springboot/controller/EmployeeController.java
@@ -1,4 +1,4 @@
-package net.javaguides.springboot.controller;
+package springboot.controller;
 
 import java.util.HashMap;
 import java.util.List;
@@ -16,11 +16,11 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import net.javaguides.springboot.exception.ResourceNotFoundException;
-import net.javaguides.springboot.model.Employee;
-import net.javaguides.springboot.repository.EmployeeRepository;
+import springboot.exception.ResourceNotFoundException;
+import springboot.model.Employee;
+import springboot.repository.EmployeeRepository;
 
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins ="*")
 @RestController
 @RequestMapping("/api/v1/")
 public class EmployeeController {
--- ems-backend/springboot-backend/src/main/java/springboot/model/Employee.java
+++ ems-backend/springboot-backend/src/main/java/springboot/model/Employee.java
@@ -1,4 +1,4 @@
-package net.javaguides.springboot.model;
+package springboot.model;
 
 import jakarta.persistence.*;
 
